6264d31de149cc7c09fe22012b2d6b07dc0d1f37
Jan Oscar Holbo Rasmussen <jan@kaddu.dk>
2017-09-28 20:35:34 +0200

    Update phoenixtrademarketlist.php
    Add a view to the component

--- site/phoenixtrademarketlist.php
+++ site/phoenixtrademarketlist.php
@@ -1,1 +1,23 @@
-Phoenix BSE Trade Market List component
+<?php
+/**
+ * Phoenix BSE Trade Market List component
+ * @package     Joomla.Administrator
+ * @subpackage  com_trademarketlist
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2017 -      Jan Holbo Rasmussen. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+
+// Get an instance of the controller prefixed by HelloWorld
+$controller = JControllerLegacy::getInstance('PhoenixTradeMarketList');
+
+// Perform the Request task
+$input = JFactory::getApplication()->input;
+$controller->execute($input->getCmd('task'));
+
+// Redirect if set by the controller
+$controller->redirect();
