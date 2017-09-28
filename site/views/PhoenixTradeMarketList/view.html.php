5c3f343b79f27777a787430da4c6d942f0f6ce21
Jan Oscar Holbo Rasmussen <jan@kaddu.dk>
2017-09-28 23:02:09 +0200

    Update view.html.php
    Add a Model to the Component

--- site/views/PhoenixTradeMarketList/view.html.php
+++ site/views/PhoenixTradeMarketList/view.html.php
@@ -28,7 +28,15 @@ class PhoenixTradeMarketListViewPhoenixTradeMarketList extends JViewLegacy
 	function display($tpl = null)
 	{
 		// Assign data to the view
-		$this->msg = 'Phoenix BSE Trade Market List';
+		$this->msg = $this->get('Msg');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			JLog::add(implode('<br />', $errors), JLog::WARNING, 'jerror');
+
+			return false;
+		}
 
 		// Display the view
 		parent::display($tpl);
